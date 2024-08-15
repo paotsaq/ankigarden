c0a89b19c50cb9542e4a3847dab11c6e0085d022
apinto @ lardafada <senhoralexandrepinto@protonmail.com>
2024-08-15 16:12:54 +0100

    some more deepL tests

--- deepl_tests.py
+++ deepl_tests.py
@@ -15,13 +15,14 @@ from os import (
 
 class TestSendPOSTRequestToRetrieveTranslation(unittest.TestCase):
 
-    # sends a good request;
+    # sends a good request with source language;
     def test_send_valid_request(self):
         """Sends a good, well formed request,
         expecting success and an ID in response"""
         target = "DA"
+        source = "EN"
         query = "This is an interesting thing"
-        success, translation = request_translation_from_api(target, query)
+        success, translation, _ = request_translation_from_api(target, query, source)
         self.assertTrue(success)
         # NOTE not exactly the best test; translations may vary...
         self.assertEqual(translation, "Dette er en interessant ting")
@@ -30,8 +31,20 @@ class TestSendPOSTRequestToRetrieveTranslation(unittest.TestCase):
     def test_send_invalid_request(self):
         target = "SILLY"
         query = "This is an interesting thing"
-        success, translation = request_translation_from_api(target, query)
+        success, translation, _ = request_translation_from_api(target, query)
         self.assertFalse(success)
+
+    # sends a good request, but without target language;
+    def test_send_valid_request_without_target(self):
+        """Sends a good, well formed request,
+        expecting success and an ID in response"""
+        target = "DA"
+        query = "This is an interesting thing"
+        success, translation, source = request_translation_from_api(target, query)
+        self.assertTrue(success)
+        self.assertEqual(source, "EN")
+        # NOTE not exactly the best test; translations may vary...
+        self.assertEqual(translation, "Dette er en interessant ting")
 
 
 if __name__ == "__main__":
