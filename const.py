32d9709f48572293e4ce58d40d6f4e258f06338c
apinto @ lardafada <senhoralexandrepinto@protonmail.com>
2024-08-19 10:09:40 +0100

    adds LANG_MAP to translate langs between services

--- const.py
+++ const.py
@@ -3,3 +3,50 @@ TRANSLATE_ENDPOINT = DEEPL_API_BASE_URL + "translate/"
 
 SOUND_API_BASE_URL = "https://api.soundoftext.com/"
 SOUNDS_ENDPOINT = SOUND_API_BASE_URL + "sounds/"
+
+LANG_MAP = {
+        "Danish": {
+            "deepl_code": "DA",
+            "sot_code": "da-DK"
+            },
+        "English": {
+            "deepl_code": "EN-US",
+            "sot_code": ""
+        }
+    }
+
+# SUPPORTED_DEEPL_TARGET = {
+        # "AR": "Arabic",
+        # "BG": "Bulgarian",
+        # "CS": "Czech",
+        # "DA": "Danish",
+        # "DE": "German",
+        # "EL": "Greek",
+        # "EN-GB": "English (British)",
+        # "EN-US": "English (American)",
+        # "ES": "Spanish",
+        # "ET": "Estonian",
+        # "FI": "Finnish",
+        # "FR": "French",
+        # "HU": "Hungarian",
+        # "ID": "Indonesian",
+        # "IT": "Italian",
+        # "JA": "Japanese",
+        # "KO": "Korean",
+        # "LT": "Lithuanian",
+        # "LV": "Latvian",
+        # "NB": "Norwegian Bokmål",
+        # "NL": "Dutch",
+        # "PL": "Polish",
+        # "PT-BR": "Portuguese (Brazilian)",
+        # "PT-PT": "Portuguese (all Portuguese variants excluding Brazilian Portuguese)",
+        # "RO": "Romanian",
+        # "RU": "Russian",
+        # "SK": "Slovak",
+        # "SL": "Slovenian",
+        # "SV": "Swedish",
+        # "TR": "Turkish",
+        # "UK": "Ukrainian",
+        # "ZH-HANS": "Chinese (simplified)",
+        # "ZH-HANT": "Chinese (traditional)"
+    # }
