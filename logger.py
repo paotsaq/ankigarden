7d1091fa47f3a69a4b50b2c05dbee9a9bdea58a2
apinto @ lardafada <senhoralexandrepinto@protonmail.com>
2024-08-19 10:10:05 +0100

    includes linebreak in logging messages

--- logger.py
+++ logger.py
@@ -2,7 +2,7 @@ import logging
 import sys
 
 # Set the basic configuration for the logger
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(funcName)s - %(filename)s:%(lineno)d - %(message)s',
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(funcName)s - %(filename)s:%(lineno)d \n %(message)s',
                     level=logging.INFO,
                     handlers=[
                         logging.FileHandler('app.log'),  # Log to a file named 'app.log'
