183dbcc525995912f01faebd601dcbb43dc6e549
apinto @ lardafada <senhoralexandrepinto@protonmail.com>
2024-09-02 11:17:07 +0200

    removes console logging

--- logger.py
+++ logger.py
@@ -2,11 +2,10 @@ import logging
 import sys
 
 # Set the basic configuration for the logger
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(funcName)s - %(filename)s:%(lineno)d \n %(message)s',
-                    level=logging.INFO,
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(funcName)s - %(filename)s:%(lineno)d \n%(message)s',
+                    level=logging.DEBUG,
                     handlers=[
                         logging.FileHandler('app.log'),  # Log to a file named 'app.log'
-                        # logging.StreamHandler(sys.stdout)  # Log to the console
                     ])
 										
 logger = logging.getLogger(__name__)
