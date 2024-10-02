2f678b66a03d34e3a87889f9b1383f556c94de07
apinto @ lardafada <senhoralexandrepinto@protonmail.com>
2024-10-02 08:01:53 +0200

    further progress in norm. routine

--- normalize_lute_terms.py
+++ normalize_lute_terms.py
@@ -12,12 +12,16 @@ from apis.lute_terms_db import (
 def normalize_lute_terms_in_database(database_path: str):
     session, engine = create_connection_to_database(database_path)
     try:
-        entries = session.query(LuteTableEntry).all()
-        for entry in entries:
+        unmatched_unnormalized_entries = (session.query(LuteTableEntry).filter(
+            LuteTableEntry.anki_note_id.is_(None) & LuteTableEntry.tags.contains('ankigarden-needs-work')).all())
+        print(len(unmatched_unnormalized_entries))
+        for entry in unmatched_unnormalized_entries:
+            print(entry)
             normalized_entry = convert_to_normalized_lute_entry(entry)
             normalized_entry.normalize()
+            print(normalized_entry)
             normalized_entry.fix_logged_problems()
-        session.commit()
+        # session.commit()
         print("Normalising process completed successfully.")
     # except Exception as e:
         # print(f"An error occurred during the matching process: {str(e)}")
